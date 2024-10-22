3d4f38c7994553d5c67f754171f632b5c00772b5
Dudchenko Arhipov <BeackDud1241@gmail.com>
2024-10-22 19:21:46 +0300

    redacted from unreading

--- mycomplex.cpp
+++ mycomplex.cpp
@@ -1,23 +1,44 @@
 #include<iostream>
 #include   <cmath>
 #include "mycomplex.h"
-using namespace std;
-Complex ::Complex(double aRe  , double   aIm){   Re = aRe;   Im = aIm; }
 
-Complex ::  Complex(const Complex& aRval)
-{  Re =aRval.Re; Im=aRval.Im;}
+using namespace std;
+
+Complex ::Complex(double aRe  , double   aIm)
+
+{   
+  Re = aRe;  
+  Im = aIm; 
+}
+
+Complex::Complex(const Complex& aRval)
+
+{  
+  Re =aRval.Re; 
+  Im=aRval.Im;
+  }
+
 Complex::~Complex() {
 Re =0.0;
-Im= 0.0;}
-void     Complex::  Set(  double aRe,double aIm) {   Re = aRe;Im = aIm;}
+Im= 0.0;
+}
+
+void     Complex::  Set(  double aRe,double aIm) 
+{   
+  Re = aRe;
+  Im = aIm;
+  }
+
 Complex::operator double()
-{  return abs();
+{  
+  return abs();
 }
 
 double Complex::abs()
 {
 return sqrt(Re*Re+Im*Im);
 }
+
 Complex Complex ::  operator+ (const Complex & aRval )
 {
   Complex Result;
--- testcmp.cpp
+++ testcmp.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
-#include     "mycomplex.h"
+#include "mycomplex.h"
+
 using namespace std;
-int main(){//тестирование класса комплексных чисел
- Complex A,B(-4), C(23.0, 45.9);
-cout<<A <<","<< B<<","<<C <<endl; A=B+C;
-cout<<"A=B+C,A="<< A<<endl;
+
+int main()
+{
+  //С‚РµСЃС‚РёСЂРѕРІР°РЅРёРµ РєР»Р°СЃСЃР° РєРѕРјРїР»РµРєСЃРЅС‹С… С‡РёСЃРµР»
+
+Complex A,B(-4), C(23.0, 45.9);
+
+cout<<A <<"A:"<< A<<", B:" <<B <<", C:"<< C << endl; 
+
+A=B+C;
+
+cout<<"A=B+C,A="<< A <<endl;
 
 Complex M=B-C;
-cout<< "M = B - C, M = " << M << endl;cout<<"M*A="<<M*A<<endl;
-cout<<"M/4.45="<<M/4.45<<endl;
 
-    Complex D;
-  	cout<<"Enter complex number D = "; cin >>D;
- A+=C+D;
-cout << "D = " << D << "\nA = " << A << endl;return 0;}
+cout<< "M = B - C, M = " << M << endl;
+
+Complex N=M*A;
+
+cout<<"N*A="<<N<<endl;
+
+Complex O =M/4.45;
+
+cout<<"O=M/4.45="<< O <<endl;
+
+Complex D;
+
+cout<<"Enter complex number D = "; cin >>D;
+
+A+=C+D;
+
+cout << "D = " << D << "\nA = " << A << endl;
+
+return 0;}
